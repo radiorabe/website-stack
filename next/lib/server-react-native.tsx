6e90755a89dda525fee8e26c00a26f1ff6c583e8
Alexandros Coutsicos <rawsix@gmail.com>
2024-11-04 11:29:54 +0200

    wip

--- next/lib/server-react-native.tsx
+++ next/lib/server-react-native.tsx
@@ -1,42 +1,42 @@
 "use client";
 
-import { Text, Pressable, TextStyle, Linking } from "react-native";
+import { Text, Pressable, View, TextStyle, Linking } from "react-native";
 
-function View({ style, children, ...otherProps }) {
-  let newStyle = Object.assign(
-    {},
-    { display: "flex", flexDirection: "column" },
-    style
-  );
+// function View({ style, children, ...otherProps }) {
+//   let newStyle = Object.assign(
+//     {},
+//     { display: "flex", flexDirection: "column" },
+//     style
+//   );
 
-  if (newStyle.paddingVertical) {
-    newStyle.paddingTop = newStyle.paddingTop
-      ? newStyle.paddingTop
-      : newStyle.paddingVertical;
-    newStyle.paddingBottom = newStyle.paddingBottom
-      ? newStyle.paddingBottom
-      : newStyle.paddingVertical;
-  }
+//   if (newStyle.paddingVertical) {
+//     newStyle.paddingTop = newStyle.paddingTop
+//       ? newStyle.paddingTop
+//       : newStyle.paddingVertical;
+//     newStyle.paddingBottom = newStyle.paddingBottom
+//       ? newStyle.paddingBottom
+//       : newStyle.paddingVertical;
+//   }
 
-  if (newStyle.paddingHorizontal) {
-    newStyle.paddingLeft = newStyle.paddingLeft
-      ? newStyle.paddingLeft
-      : newStyle.paddingHorizontal;
-    newStyle.paddingRight = newStyle.paddingRight
-      ? newStyle.paddingRight
-      : newStyle.paddingHorizontal;
-  }
+//   if (newStyle.paddingHorizontal) {
+//     newStyle.paddingLeft = newStyle.paddingLeft
+//       ? newStyle.paddingLeft
+//       : newStyle.paddingHorizontal;
+//     newStyle.paddingRight = newStyle.paddingRight
+//       ? newStyle.paddingRight
+//       : newStyle.paddingHorizontal;
+//   }
 
-  return (
-    <div
-      {...otherProps}
-      style={newStyle}
-      onClick={otherProps.onPress ? otherProps.onPress : undefined}
-    >
-      {children}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       {...otherProps}
+//       style={newStyle}
+//       onClick={otherProps.onPress ? otherProps.onPress : undefined}
+//     >
+//       {children}
+//     </div>
+//   );
+// }
 
 // const View = ({ style, children, ...otherProps }) =>
 //   <div style={Object.assign({ marginRight: "1.5em" }, style)} {...otherProps}>
