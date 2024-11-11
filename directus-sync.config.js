9e9ea83fa9a88dd3e54d17bd798363f4789be2d4
Alexandros Coutsicos <rawsix@gmail.com>
2024-11-11 13:39:08 +0200

    Sync Data between instances

---
+++ directus-sync.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  directusUrl: "http://localhost:1337",
+  directusToken: "LpjqomxFZHWlj_-tEmS7CZ6zLwVHb52Y",
+  // directusUrl: "https://staging-data.rabe.ch",
+  // directusToken: "OzmYfob0RPyWjgiR0a8fizDydt1tj4ly",
+  dumpPath: "./directus-config",
+  // Additional options...
+};
