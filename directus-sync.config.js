6b3698d5529a45ddc94c6628af47fc51f5717596
Alexandros Coutsicos <rawsix@gmail.com>
2024-11-22 10:41:20 +0200

    Sync

--- directus-sync.config.js
+++ directus-sync.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  directusUrl: "http://localhost:1337",
-  directusToken: "LpjqomxFZHWlj_-tEmS7CZ6zLwVHb52Y",
-  // directusUrl: "https://staging-data.rabe.ch",
-  // directusToken: "OzmYfob0RPyWjgiR0a8fizDydt1tj4ly",
+  directusUrl: "http://localhost:1337", // dev local
+  directusToken: "LpjqomxFZHWlj_-tEmS7CZ6zLwVHb52Y", // dev local
   dumpPath: "./directus-config",
   // Additional options...
 };
+
+// needs to use other config file to work properly with staging and preproduction
+// directusUrl: "https://staging-data.rabe.ch", // staging
+// directusToken: "OzmYfob0RPyWjgiR0a8fizDydt1tj4ly", // staging
+// directusUrl: "https://preproductiondata.rabe.ch", // preproduction
+// directusToken: "nyxxy5-kazMon-gusvuk", // preproduction
