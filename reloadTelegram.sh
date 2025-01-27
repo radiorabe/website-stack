eec881bb39659fc03f9c2f46d74eb97e9c7a97b5
Alexandros Coutsicos <mail@aarefabrik.ch>
2025-01-27 10:06:50 +0000

    reload in BE

--- reloadTelegram.sh
+++ reloadTelegram.sh
@@ -1,3 +1,3 @@
-docker compose stop telegram-notifier
-docker compose rm -f telegram-notifier
+# docker compose stop telegram-notifier
+# docker compose rm -f telegram-notifier
 docker compose up -d --no-deps --build telegram-notifier
