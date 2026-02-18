bcd50b9d67da5dadfea601f52b0bede9aa458674
Alexandros Coutsicos <mail@aarefabrik.ch>
2026-02-18 21:24:51 +0000

    Fix old cached files on restart

--- reloadNext.sh
+++ reloadNext.sh
@@ -1,5 +1,6 @@
 docker compose stop next
 rm -rf next/.next
-# docker compose rm -f next
-# docker-compose build --no-cache next # for rebuilding whole dockerfile (if .env changes)
-docker compose up -d --no-deps --build --force-recreate next
+docker compose rm -f next
+docker compose build --no-cache next # for rebuilding whole dockerfile (if .env changes)
+# docker compose up -d --no-deps --build --force-recreate next
+docker compose up -d --no-deps next
